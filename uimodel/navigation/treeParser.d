58c9c97fd3d071ff73d9d84c7047f5792aa46ce5
Johannes Teichrieb <isrvoid@gmail.com>
2016-02-13 17:10:16 +0100

    Add normalizeIndentation(), refactor [treeParser]

--- uimodel/navigation/treeParser.d
+++ uimodel/navigation/treeParser.d
@@ -11,16 +11,27 @@ import uimodel.navigation.tree;
 
 @safe:
 /*
-   TODO mostly to address SR at various places
-   - implement normalize and verify
-   - remove mentioning of indentationSize
-   - simplify lower hierarchy to foreach line : line = line[1 .. $];
-   - remove isWhite checks from stripIndentation -- rename it to lowerHierarchyLevel
+   TODO
+   - implement verify
    - TreeParserException
    - move Msg enum into TreeParserException
    - inherit UITreeNode from TreeNode
    - remove whitespace check from splitRoots -- input will have been verified
    */
+
+TreeNode[] parseTrees(string text)
+{
+    auto lines = text.normalizeAndVerifyInput().array();
+    Appender!(TreeNode[]) trees;
+    foreach (treeLines; splitRoots(lines))
+    {
+        auto tree = parseTreeLines(null, treeLines);
+        trees.put(tree);
+    }
+    return trees.data;
+}
+
+private:
 
 private enum Msg : string
 {
@@ -29,54 +40,29 @@ private enum Msg : string
     hierarchyError = "Input results in invalid hierarchy"
 }
 
-TreeNode[] parseTrees(size_t indentationSize = 4)(string text)
-{
-    static assert(indentationSize > 0);
-
-    auto lines = text.normalizeAndVerifyInput().array();
-    Appender!(TreeNode[]) trees;
-    foreach (treeLines; splitRoots(lines))
-    {
-        auto tree = parseTreeLines!indentationSize(null, treeLines);
-        trees.put(tree);
-    }
-    return trees.data;
-}
-
-private:
-
-TreeNode parseTreeLines(size_t indentationSize)(TreeNode root, string[] lines)
+TreeNode parseTreeLines(TreeNode root, string[] lines)
 {
     if (lines.empty)
         return null;
 
-    bool firstLineBeginsWithWhitespace = isWhite(lines[0][0]);
-    if (firstLineBeginsWithWhitespace)
-        throw new Exception(Msg.rootIsIndented);
-
-    auto node = new TreeNode(root, new UIObject(lines[0]));
+    auto node = new TreeNode(root, new UIObject(lines.front));
+    lines.popFront();
+    // lower hierarchy level by popping front chars
+    lines.each!((ref a) => a = a[1 .. $]);
 
     Appender!(TreeNode[]) children;
-    auto lowerHierarchyLevel = lines[1 .. $].stripIndentation!indentationSize.array();
-    foreach(lowerRoot; splitRoots(lowerHierarchyLevel))
-        children.put(parseTreeLines!indentationSize(node, lowerRoot));
+    foreach(lowerRoot; splitRoots(lines))
+        children.put(parseTreeLines(node, lowerRoot));
 
     node.children = children.data;
     return node;
 }
 
-// FIXME remove after making parseTreeLinex expect normalized checked input
-// tree can't have multiple roots
-unittest
-{
-    assertThrown(parseTreeLines!4(null, ["foo", "bar"]));
-}
-
 string[] normalizeAndVerifyInput(string s) pure
 {
     auto lines = s.getNonEmptyLinesStrippedOfTrailingWhitespace().array();
-    // FIXME normalize indentation
-    // FIXME check hierarchy correctness
+    lines = normalizeIndentation(lines);
+    // TODO check hierarchy correctness
     return lines;
 }
 
@@ -86,25 +72,83 @@ auto getNonEmptyLinesStrippedOfTrailingWhitespace(string s) pure
     return s.splitLines().map!(a => a.stripRight()).filter!(a => !a.empty);
 }
 
-auto stripIndentation(size_t indentationSize, T)(T lines) pure nothrow
-    if (isIterable!T && isSomeString!(ElementType!T))
+/*
+   TODO
+   - move to util
+   - Feature: Add check for whitespace characters consistency.
+     Whitespaces are considered equal and counted as 1 -- potentially deceptive.
+*/
+string[] normalizeIndentation(string[] _lines) pure
+in
 {
-    return lines.map!((a)
-        {
-            if (a.length < indentationSize || !isWhite(a[0 .. indentationSize]))
-                throw new Exception(Msg.indentationError);
+    foreach (line; _lines)
+        assert(line.hasNonWhitespaceChar());
+}
+body
+{
+    auto lines = _lines.dup;
+    auto indentSizes = lines.map!(a => a.getLeadingWhitespaceCount()).array();
+    auto nonZeroIndentSizes = indentSizes.filter!(a => a > 0);
+    auto minNonZeroIndentSize = reduce!min(size_t.max, nonZeroIndentSizes);
+    alias normDivider = minNonZeroIndentSize;
+    foreach (i, ref line; lines)
+    {
+        auto indentSize = indentSizes[i];
+        if (indentSize % normDivider)
+            throw new Exception(Msg.indentationError);
 
-            return a[indentationSize .. $];
-        });
+        auto newIndentSize = indentSize / normDivider;
+        line = line[indentSize - newIndentSize .. $];
+    }
+    return lines;
 }
 
-bool isWhite(string s) pure nothrow
+size_t getLeadingWhitespaceCount(string s) pure nothrow
 {
-    foreach(c; s)
+    foreach (i, c; s)
         if (!c.isWhite())
-            return false;
+            return i;
 
-    return true;
+    return s.length;
+}
+
+unittest // empty input yields null
+{
+    assert(null is normalizeIndentation(null));
+    assert(null is normalizeIndentation([]));
+}
+
+unittest // unindented input is left unchanged
+{
+    enum lines = ["foo", "bar"];
+    assert(lines == normalizeIndentation(lines));
+}
+
+unittest // normalized indentation is left unchanged
+{
+    enum lines = [" foo", "bar", "   fun"];
+    assert(lines == normalizeIndentation(lines));
+}
+
+unittest // simple normalization
+{
+    assert([" foo"] == normalizeIndentation(["    foo"]));
+    assert([" foo", "bar", "  fun"] == normalizeIndentation(["   foo", "bar", "      fun"]));
+}
+
+unittest // inconsistent indentation error
+{
+    assertThrown(normalizeIndentation(["  foo", "   bar"]));
+    assertThrown(normalizeIndentation(["    foo", "     bar", "  fun"]));
+}
+
+bool hasNonWhitespaceChar(string s) pure nothrow
+{
+    foreach (c; s)
+        if (!c.isWhite())
+            return true;
+
+    return false;
 }
 
 string[][] splitRoots(string[] lines)
@@ -146,9 +190,9 @@ version (unittest)
         return (null is n.parent && n.children.empty);
     }
 
-    TreeNode parseTree(size_t indentationSize = 4)(string text)
+    TreeNode parseTree(string text)
     {
-        auto trees = parseTrees!indentationSize(text);
+        auto trees = parseTrees(text);
         if (null is trees)
             return null;
 
@@ -214,7 +258,7 @@ unittest
 // three nodes
 unittest
 {
-    auto prev = parseTree!1("foo\n bar\n  fun");
+    auto prev = parseTree("foo\n bar\n  fun");
     assert(1 == prev.children.length);
     auto next = prev.children[0];
     assert("bar" == next.obj.id);
@@ -230,16 +274,16 @@ unittest
 // two children tree
 unittest
 {
-    auto root = parseTree!1("foo\n bar\n fun");
+    auto root = parseTree("foo\n bar\n fun");
     assert(2 == root.children.length);
 }
 
 // hierarchy error
 unittest
 {
-    assertThrown(parseTrees!1(" foo"));
-    assertThrown(parseTrees!1(" foo\nbar"));
-    assertThrown(parseTrees!1("foo\n  bar\n fun"));
+    assertThrown(parseTrees(" foo"));
+    assertThrown(parseTrees(" foo\nbar"));
+    assertThrown(parseTrees("foo\n  bar\n fun"));
 }
 
 version (unittest)
@@ -277,7 +321,7 @@ unittest
 
 3";
     enum childrenCount = [3, 2, 1, 0];
-    auto trees = parseTrees!1(input);
+    auto trees = parseTrees(input);
     assert(4 == trees.length);
     foreach (i, tree; trees)
     {
